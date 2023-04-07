fc4eadbceb66ae7a27d6db865923f2060eb9be57
afifah.nafis <afifah.nafis@meteor.id>
2023-04-07 18:58:55 +0700

    add .well-known/assetlinks.json

--- pages/api/assetlinks.ts
+++ pages/api/assetlinks.ts
@@ -8,9 +8,9 @@ export default function handler(
     relation: ["delegate_permission/common.handle_all_urls"],
     target: {
       namespace: "android_app",
-      package_name: "com.example.app",
+      package_name: "id.meteor.alfamind_dev",
       sha256_cert_fingerprints: [
-        "11:22:33:44:55:66:77:88:99:00:AA:BB:CC:DD:EE:FF:11:22:33:44:55:66:77:88:99:00:AA:BB:CC:DD:EE:FF",
+        "2B:13:4E:18:2E:4F:E6:46:75:9B:3F:75:BB:BE:28:B2:A4:F2:90:C9:D2:22:34:CF:0B:58:4B:E8:EB:3A:5B:25",
       ],
     },
   });

18bced77ea5859ad0f58f4f9f98e23a849f0fba5
afifah.nafis <afifah.nafis@meteor.id>
2023-04-10 11:27:15 +0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,61 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: "default-src 'self'",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+  {
+    key: "Access-Control-Allow-Origin",
+    value: "*",
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/.well-known/assetlinks.json",
+  //       destination: "/api/assetlinks",
+  //     },
+  //   ];
+  // },
+  async headers() {
     return [
       {
         source: "/.well-known/assetlinks.json",
-        destination: "/api/assetlinks",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
       },
+      // {
+      //   source: "/(.*)",
+      //   headers: securityHeaders,
+      // },
     ];
   },
 };

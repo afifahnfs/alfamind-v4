17f6f8606a52bf270747491f4c15f5b2f66cb22a
afifah.nafis <afifah.nafis@meteor.id>
2023-04-10 11:36:51 +0700

    add meta in index.tsx

--- next.config.js
+++ next.config.js
@@ -1,61 +1,12 @@
 /** @type {import('next').NextConfig} */
-
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: "default-src 'self'",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "DENY",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
-  {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=()",
-  },
-  {
-    key: "Access-Control-Allow-Origin",
-    value: "*",
-  },
-];
-
 const nextConfig = {
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/.well-known/assetlinks.json",
-  //       destination: "/api/assetlinks",
-  //     },
-  //   ];
-  // },
-  async headers() {
+  async rewrites() {
     return [
       {
         source: "/.well-known/assetlinks.json",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/json",
-          },
-        ],
+        destination: "/api/assetlinks",
       },
-      // {
-      //   source: "/(.*)",
-      //   headers: securityHeaders,
-      // },
     ];
   },
 };

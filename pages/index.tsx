1b049f2293f464d95dc2912e90f68a5ffd4eadff
afifah.nafis <afifah.nafis@meteor.id>
2023-08-02 18:39:37 +0700

    fix: direct link to playtore if open in browser

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,12 @@ import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  if (isBrowser) {
+  useEffect(() => {
     // Redirect to the Play Store URL for Android devices
     window.location.href =
       "https://play.google.com/store/apps/details?id=id.meteor.alfamind&pli=1";
-    return null; // Return null to prevent rendering anything on the page
-  }
+    // return null; // Return null to prevent rendering anything on the page
+  });
 
   return (
     <>

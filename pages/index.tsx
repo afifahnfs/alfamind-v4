10e02da7f32e829c1db133c32f5992b1d5c3a470
afifah.nafis <afifah.nafis@meteor.id>
2023-08-02 17:58:40 +0700

    feat: adding & implement in browser react-device-detect

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,21 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { isBrowser } from "react-device-detect";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    if (isBrowser) {
+      // Redirect to the Play Store URL for Android devices
+      window.location.href =
+        "https://play.google.com/store/apps/details?id=id.meteor.alfamind&pli=1";
+      return null; // Return null to prevent rendering anything on the page
+    }
+  }, []);
+
   return (
     <>
       <Head>
